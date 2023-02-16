959da4370bad92be9c16ba57c35a158fd01268ea
GerBull <da.geraskin@gmail.com>
2023-02-16 15:16:05 +0300

    All ready for pushing

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
@@ -49,39 +48,105 @@ $(document).ready(function(){
 		})
 	});
 
+
     //Validate
-  $('#consultation-form').validate();
-  $('#consultation form').validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 2
+
+  function validateForms(form){
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: {
+          required: true,
+          minlength: 10,
+        },
+        email: {
+          required: true,
+          email: true
+        }
       },
-      phone: {
-        required: true,
-        minlength: 11,
-        maxlength: 11
-      },
-      email: {
-        required: true,
-        email: true
+      messages: {
+        name: {
+          required: "Пожалуйста, укажите свое имя",
+          minlength: jQuery.validator.format("Хотя бы {0} символа требуется!")
+        },
+        phone: {
+          required: "Пожалуйста, укажите свой телефон",
+          minlength: jQuery.validator.format("Телефон должен состоять из {0} цифр")
+        },
+        email: {
+          required: "Пожалуйста, укажите свой email",
+          email: "Введите почту в виде ivan@yandex.ru"
+        }     
       }
-    },
-    messages: {
-      name: {
-        required: "Пожалуйста, укажите свое имя",
-        minlength: jQuery.validator.format("Хотя бы {0} символа требуется!")
-      },
-      phone: {
-        required: "Пожалуйста, укажите свой телефон",
-        minlength: jQuery.validator.format("Телефон должен состоять из {0} цифр"),
-        maxlength: jQuery.validator.format("Телефон должен состоять из {0} цифр")
-      },
-      email: {
-        required: "Пожалуйста, укажите свой email",
-        email: "Введите почту в виде ivan@yandex.ru"
-      }     
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+
+  //Mask input
+  
+  $('input[name=phone]').mask("+7 (999) 999-9999",{placeholder:"X"},{autoclear: false})
+
+
+  //Server sending
+
+  $('form').submit(function(e) {
+    if (!$(this).valid()) {
+      return
+    }
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done (function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+
+
+  //Smooth scroll and page up
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 800) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
     }
   });
-  $('#order form').validate();
+
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 200, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+  new WOW().init();
+
 });
